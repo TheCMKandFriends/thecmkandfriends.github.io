05fdf13b251c7097e36fc0a36add246e1826102f
feuerle <feuerle.claudia@gmail.com>
2024-10-26 22:34:33 +0200

    change

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const navMenu = document.querySelector('.nav-menu');
-
-mobileMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
